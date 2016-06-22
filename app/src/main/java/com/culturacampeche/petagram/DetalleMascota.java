e2a965267bcf298465e258ab094024baab1e974f
Alejandro Ku <alejandro.ku@gmail.com>
2016-06-22 12:15:41 -0500

    Actualizado

--- app/src/main/java/com/culturacampeche/petagram/DetalleMascota.java
+++ app/src/main/java/com/culturacampeche/petagram/DetalleMascota.java
@@ -1,12 +1,8 @@
 package com.culturacampeche.petagram;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by bibliotecacampeche on 22/06/16.
- */
 public class DetalleMascota extends AppCompatActivity {
 
     @Override
