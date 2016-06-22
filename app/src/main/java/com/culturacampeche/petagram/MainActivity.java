7f01887a5e11f287c08ae1e14f8ebca4a8fd3373
Alejandro Ku <alejandro.ku@gmail.com>
2016-06-22 14:37:45 -0500

    Actualizado

--- app/src/main/java/com/culturacampeche/petagram/MainActivity.java
+++ app/src/main/java/com/culturacampeche/petagram/MainActivity.java
@@ -1,7 +1,11 @@
 package com.culturacampeche.petagram;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    public void agregarFAB() {
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, getResources().getString(R.string.mensaje), Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
